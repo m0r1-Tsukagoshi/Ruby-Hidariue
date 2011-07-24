19bd47138849e0a278336d2ec85b62fc071c439d
Tatsuhiro Tsukagoshi <tmoridog@gmail.com>
2011-07-24 19:00:54 +0900

    取得範囲選択機能追加 ver1.01

--- hidariue.rb
+++ hidariue.rb
@@ -8,17 +8,62 @@ def getFileName(num)
     zeroPad(num) + ".gif"
 end
 
-def zeroPad(x)
-    "0" * (3 - x.to_s.size) + x.to_s
+def zeroPad(num)
+    "0" * (3 - num.to_s.size) + num.to_s
 end
 
-Dir.mkdir(DIRNAME) unless File.exist?(DIRNAME)
+def argChk
 
-(0..999).each{|i|
-    open(DIRNAME + "/" + getFileName(i), "w") do |output|
-        open(URL + getFileName(i)) do |data|
-            output.write(data.read)
+    if(ARGV[0].to_s=="-h" || ARGV[0].to_s=="-help")
+        puts <<-DOC
+
+ruby hidariue.rb [fileNumber1] [fileNumber2]
+
+fileNumbers 0 - 999
+
+ruby hidariue.rb
+    you get 000.gif - 999.gif
+
+ruby hidariue.rb 100
+    you get 100.gif
+
+ruby hidariue.rb 10 20
+    you get 010.gif - 020.gif
+
+ruby hidariue.rb -help
+    help me erin ( ﾟ∀ ﾟ)o彡゜えーりん！えーりん！
+
+        DOC
+        exit
+    end
+
+    min = ARGV[0].nil? ? 0 : ARGV[0].to_i
+    max = ARGV[1].nil? ? ARGV[0].nil? ? 999 : ARGV[0].to_i : ARGV[1].to_i
+    if(999< min || min < 0 || 999< max || max < 0 || max < min)
+        puts <<-DOC
+
+hidariue.rb is ArgumentError
+ruby hidariue.rb -help
+
+        DOC
+        exit
+    end
+    
+    Dir.mkdir(DIRNAME) unless File.exist?(DIRNAME)
+    
+    return min,max
+end
+
+
+min,max = argChk
+(min..max).each{|num|
+    open(DIRNAME + "/" + getFileName(num), "w") do |gifFile|
+        open(URL + getFileName(num)) do |data|
+            gifFile.write(data.read)
         end
     end
     sleep 0.01
 }
+
+__END__
+
